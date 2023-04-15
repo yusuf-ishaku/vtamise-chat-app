5e8a6050b6f8ec6ddf27ade3d60da7a3ba61259e
yusuf <kingshakzy@gmail.com>
2023-04-15 11:44:45 +0100

    Installing dependencies

---
+++ src/pages/login-page.jsx
@@ -0,0 +1,5 @@
+export const LoginPage = () =>{
+    return(
+        <div> This is the login page</div>
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./app.jsx"
+  ],
   theme: {
     extend: {},
   },
